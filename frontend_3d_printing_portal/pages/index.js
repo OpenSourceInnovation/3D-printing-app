b26a742628cdda2ddb719c247145d420d9efc8d7
Amn04 <abk8088@gmail.com>
2023-01-25 00:20:57 +0530

    Landing Page created in frontend

--- frontend_3d_printing_portal/pages/index.js
+++ frontend_3d_printing_portal/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
+import { HomePage } from '@/component/landing_page/home'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <HomePage/>
     </>
   )
 }
