2bbc1af45b9a04e28abfaf3c725c63837d6ec0c9
Pravesh99179 <praveshagrawal2020@gmail.com>
2023-01-25 23:25:45 +0530

    modified color

--- frontend_3d_printing_portal/src/component/Navbar.jsx
+++ frontend_3d_printing_portal/src/component/Navbar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
   const [navbar, setNavbar] = useState(false);
   return (
     <div>
-      <nav className="w-full bg-green-500 fixed top-0 left-0 right-0 z-10">
+      <nav className="w-full bg-green-600 fixed top-0 left-0 right-0 z-10">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
