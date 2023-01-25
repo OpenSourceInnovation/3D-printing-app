1b4fe35c688d6e8f283071c9b369eada8738a7b5
Tushargupta9800 <tushargupta9800@gmail.com>
2023-01-26 00:25:57 +0530

    added the apis for guest login

--- backend_3d_printing/app.js
+++ backend_3d_printing/app.js
@@ -1,11 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
-
-const url = 'mongodb+srv://threedprinting:threedprinting@three-d-printing-model.uvpiynj.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(url);
 
 app.use(cors());
 
@@ -13,23 +8,7 @@ app.use(cors());
 const companyAuth = require('./company/authentication');
 app.use('/company', companyAuth);
 
-//middleware
-// app.use('/', () => {
-//     console.log("middle ware running");
-// });
-
-
-//routes
-app.get('/company', (req, res) => {
-    res.send("we are home in home");
-});
-
-//connected to mongodb
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb+srv://threedprinting:threedprinting@three-d-printing-model.uvpiynj.mongodb.net/?retryWrites=true&w=majority', () => {
-    console.log("connected to db");
-});
-
-//listening to the port
+const serviceProvider = require('./service_provider/authentication');
+app.use('/serviceprovider', serviceProvider);
 
 app.listen(3000);
--- backend_3d_printing/service_provider/authentication.js
+++ backend_3d_printing/service_provider/authentication.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/auth/guest/:email', async (req, res) => {
 
     email = req.params.email;
-    const _collection = await mongodbCollection("company", "authentication");
+    const _collection = await mongodbCollection("service_provider", "authentication");
     var cursor = await _collection.findOne({"email" : email});
     if(cursor == null){
         await _collection.insertOne({"email" : email});
