7dacc8a3215512de6c7d043b2a2a0b24a8a524bb
Tushargupta9800 <tushargupta9800@gmail.com>
2023-01-25 20:48:39 +0530

    removed the python django backend with js using express(node)

--- backend_3d_printing/api/company/company_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import authentication
-
-#Endpoints are mapped in this file
-urlpatterns = [
-    path('guest/login/<email>',authentication.guest_login_company),
-]
